1415af8a728bd8f2d3942b96965bc86c61d0c91f
Sedrac Lucas Calupeteca <slcsedrac@gmail.com>
2023-05-08 20:45:26 +0100

    Classes fail

--- src/main/java/com/ei/math/function/Function.java
+++ src/main/java/com/ei/math/function/Function.java
@@ -13,10 +13,9 @@ import lombok.NoArgsConstructor;
 public class Function {
     @Builder.Default
     private Fraction coefficient = Fraction.of();
+    private String ingonita;
     @Builder.Default
-    private String ingonita = "";
-    @Builder.Default
-    private Fraction expoent = Fraction.of();
+    private Fraction expoent = Fraction.of(1);
     
     public Function of(Fraction coefficient, String ingonita, Fraction expooent){
         return  Function.builder().coefficient(coefficient).ingonita(ingonita).expoent(expoent).build();
@@ -32,20 +31,6 @@ public class Function {
 
     public Function of(String ingonita){
         return  of(Fraction.of(1), ingonita, Fraction.of(1));
-    }
-    
-    public Function root() {
-        return new Function(Fraction.of(), ingonita, expoent);
-    }
-    
-    public boolean equalsFunction(Function function){
-        if(!expoent.equals(function.getExpoent())) return false;
-        if(!ingonita.equals(function.getIngonita())) return false;
-        return coefficient.equals(function.getCoefficient());
-    }
-    
-    public String simplyText(){
-        return String.format("%s^%s", ingonita, expoent.text());
-    }
+    }    
     
 }
